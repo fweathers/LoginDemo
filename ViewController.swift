b817592575118763f5153172558e9657537991f4
Felicia Weathers <Fel.Weathers@gmail.com>
2016-10-14 02:25:44 -0400

    Check if there is a username logged in

--- ViewController.swift
+++ ViewController.swift
@@ -7,12 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    @IBAction func logInButtonTapped(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            for result in results as! [NSManagedObject] {
+                
+                if let username = result.value(forKey: "name") as? String {
+                    
+                    textField.alpha = 0
+                    
+                    logInButton.alpha = 0
+                    
+                    label.alpha = 1
+                    
+                    label.text = "Hi there " + username + "!"
+                }
+                
+            }
+            
+        } catch {
+            
+            print("Request failed")
+        }
+    
     }
 
     override func didReceiveMemoryWarning() {
