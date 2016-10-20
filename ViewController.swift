49441864a82b8687882048b33272b36260a4993e
Felicia Weathers <Fel.Weathers@gmail.com>
2016-10-20 15:22:31 -0400

    User abe to update username and log out

--- ViewController.swift
+++ ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 import CoreData
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
+    
+    var isLoggedIn = false
     
     @IBAction func logoutButtonTapped(_ sender: UIButton) {
         
@@ -49,9 +51,12 @@ class ViewController: UIViewController {
                 
                 logoutButton.alpha = 0
                 
+                logInButton.setTitle("Log in", for: [])
+                
                 textField.alpha = 1
                 
-                logInButton.alpha = 1
+                isLoggedIn = false
+                
             }
             
         } catch {
@@ -68,35 +73,74 @@ class ViewController: UIViewController {
         
         let context = appDelegate.persistentContainer.viewContext
         
-        // Save a new item in an entity
-        
-        let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
-        
-        newValue.setValue(textField.text, forKey: "name")
-        
-        do {
+        if isLoggedIn {
             
-            try context.save()
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
             
-            textField.alpha = 0
+            do {
+                
+                let results = try context.fetch(request)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        setValue(textField.text, forKey: "name")
+                        
+                        do {
+                            
+                            try context.save()
+                            
+                        } catch {
+                            
+                            print("Update username save failed")
+                            
+                        }
+                        
+                    }
+                    
+                    label.text = "Hi there" + textField.text! + "!"
+                }
+                
+            } catch {
+                
+                print("Update username failed")
+                
+            }
+        } else {
             
-            logInButton.alpha = 0
+            // Save a new item in an entity
             
-            label.alpha = 1
-
-            label.text = "Hi there " + textField.text! + "!"
-
+            let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
             
-        } catch {
+            newValue.setValue(textField.text, forKey: "name")
             
-            print("Failed to save")
+            do {
+                
+                try context.save()
+                
+                logInButton.setTitle("Update username", for: [])
+                
+                logoutButton.alpha = 1
+                
+                label.alpha = 1
+                
+                label.text = "Hi there " + textField.text! + "!"
+                
+                isLoggedIn = true
+                
+                
+            } catch {
+                
+                print("Failed to save")
+            }
         }
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         
         let context = appDelegate.persistentContainer.viewContext
@@ -114,10 +158,10 @@ class ViewController: UIViewController {
             for result in results as! [NSManagedObject] {
                 
                 if let username = result.value(forKey: "name") as? String {
+                                        
+                    logoutButton.alpha = 1
                     
-                    textField.alpha = 0
-                    
-                    logInButton.alpha = 0
+                    logInButton.setTitle("Update username", for: [])
                     
                     label.alpha = 1
                     
@@ -130,14 +174,14 @@ class ViewController: UIViewController {
             
             print("Request failed")
         }
-    
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
